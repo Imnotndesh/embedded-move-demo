b166718e5b393740ed723aa22765d592287a8115
Brian <slateinbox@gmail.com>
2025-12-01 14:21:06 +0300

    YAAY WE CAN PRINT NOW

--- src/main.c
+++ src/main.c
@@ -1,48 +1,42 @@
-#include <stdio.h> // Required for printf
+#include <stdio.h> 
 
-// Manual definitions of Special Function Registers (SFRs)
-// This avoids relying on the complex <mcs51/8052.h> header
-__sfr __at(0x88) TCON; // Timer Control
-__sfr __at(0x89) TMOD; // Timer Mode
-__sfr __at(0x8b) TL1;  // Timer 1 Low
-__sfr __at(0x8d) TH1;  // Timer 1 High
+__sfr __at(0x88) TCON;
+__sfr __at(0x89) TMOD;
+__sfr __at(0x8d) TH1;
+__sfr __at(0x98) SCON;
+__sfr __at(0x99) SBUF;
 
-__sfr __at(0x98) SCON; // Serial Control
-__sfr __at(0x99) SBUF; // Serial Buffer
+void init_uart_9600() 
+{
+    TMOD |= 0x20; 
+    SCON = 0x50;  
+    TH1 = 0xFD; 
+    TCON |= 0x40; 
+    SCON |= 0x02;               
+}
 
-// --- Overriding putchar for printf ---
-// This is the implementation from the forum post, optimized for SDCC
+void delay_ms(unsigned int ms) {
+    unsigned int i, j;
+    for (i = 0; i < ms; i++) {
+        for (j = 0; j < 114; j++); 
+    }
+}
+
 int putchar(int c)
 {
-	// Wait until TI (Transmit Interrupt, SCON bit 1, value 0x02) is set
 	while(!(SCON & 0x02)); 
-	SCON &= ~0x02; // Clear TI flag
-	SBUF = c;
+	SCON &= ~0x02;         
+	SBUF = c;              
 	return (c);
 }
 
 void main(void)
 {
-	unsigned long int i;
+    init_uart_9600();
 
-	// 1. Configure Timer 1 for 9600 Baud on 11.0592MHz Crystal
-	TMOD = 0x20;   // Timer 1, Mode 2 (8-bit auto-reload)
-	SCON = 0x50;   // Serial Mode 1 (8-bit UART), REN=1 (Receive Enable)
-    
-	// 2. Set reload value (0xFD = 253, calculated for 9600 baud)
-	TH1 = 0xFD; 
-	
-	// 3. Start Timer 1
-	TCON |= 0x40; // TCON bit 6 is TR1 (Timer 1 Run control)
-    
-	// 4. Set TI flag to 1 so the first putchar can run immediately
-	SCON |= 0x02;				
-
-	for(;;)
-	{
-		printf("SUCCESS: Upload works! Handshake achieved!\n");
-        
-        // Delay loop from the tutorial (approx 1 second)
-		for(i = 0; i < 147456; i++); 
-	}
+    while (1)
+    {
+        printf("Hello There :)\n");
+        delay_ms(5000); 
+    }
 }
